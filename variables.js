107b36980cb97d513d72c30d67f573603ae8aa69
MaryMafa <marymafa80@gmail.com>
2019-03-13 10:01:21 +0000

    created functions and variables

---
+++ variables.js
@@ -0,0 +1,33 @@
+// my 3 functions
+function addition(value1, value2, value3) {
+    var answer = value1 + value2 + value3;
+    return answer;
+}
+
+function substract(value4, value8){
+var answer = value4 - value8;
+return answer;
+}
+
+function multiplication( value5, value3){
+var answer = value5 % value3;
+    return answer;
+}
+
+// here are my 9 variables 
+var three = 3;
+var five = 5;
+var addedAnswer = three + five;
+var seven = 7;
+var two = 2;
+var substractedAnswer = seven - two;
+var nine = 9;
+var one = 1;
+var multipliedAnswer = nine * one;
+
+console.log(five)
+
+
+console.log(addition(4, 8, 9));
+console.log(substract(4, 8));
+console.log( multiplication(5, 3));
