f74b96df11f2122fef46defe81f57e3fe00c668a
MaryMafa <marymafa80@gmail.com>
2019-04-09 12:07:17 +0000

    added a history array

--- ATM.js
+++ ATM.js
@@ -1,13 +1,21 @@
 var balance = 2000;
-
+var withdrawalHistory = [];
 function giveUserBalance() {
     return "your current balance " + balance;
 }
 function withdraw(withdrawalAmount) {
     if (balance < withdrawalAmount) {
         return "insufficient funds";
+        
     } else {
         balance = balance - withdrawalAmount;
+        withdrawalHistory.push(withdrawalAmount)
         return "withdrawal of " + withdrawalAmount + " successful";
     }
 }
+console.log(giveUserBalance());
+console.log(withdraw(1000));
+console.log(giveUserBalance());
+console.log(withdraw(500));
+console.log(withdraw(700));
+console.log(withdrawalHistory);
