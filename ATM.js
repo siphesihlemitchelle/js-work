b2cb1d07396793edbcc0296355550c06922aae99
MaryMafa <marymafa80@gmail.com>
2019-04-09 11:55:46 +0000

    created withdraw function

--- ATM.js
+++ ATM.js
@@ -3,3 +3,11 @@ var balance = 2000;
 function giveUserBalance() {
     return "your current balance " + balance;
 }
+function withdraw(withdrawalAmount) {
+    if (balance < withdrawalAmount) {
+        return "insufficient funds";
+    } else {
+        balance = balance - withdrawalAmount;
+        return "withdrawal of " + withdrawalAmount + " successful";
+    }
+}
